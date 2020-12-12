44d4d0aa3d575c170539f08c76c69029627fd57c
João Eliandro <eliandrogermano@gmail.com>
2020-12-12 20:53:13 -0300

    Add status route

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,9 +6,25 @@ const routes = Router();
 routes.get('/', (request, response) =>
   response.json({ message: "Hello World!" }));
 
-routes.post('/accounts', async (request, response) => {
+routes.get('/status', async (request, response) => {
+  return response.json({
+    message: "Operação realizada com sucesso.",
+    code: 0
+  });
+});
 
-  return response.json(result);
+routes.post('/purchases', async (request, response) => {
+  console.log(request.body);
+
+  return response.json({ ok: "ok" });
+});
+
+routes.post('/purchases/cancel', async (request, response) => {
+  return response.json({ ok: "ok" });
+});
+
+routes.post('//chargebacks', async (request, response) => {
+  return response.json({ ok: "ok" });
 });
 
 module.exports = routes;
