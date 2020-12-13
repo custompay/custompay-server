e610ccf29ee8e77c541a136d0f0f7db5eaae0d21
João Eliandro <eliandrogermano@gmail.com>
2020-12-13 03:53:24 -0300

    Customized routes for callbacks

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,16 @@ const api = require('../services/api');
 
 const routes = Router();
 
+const authorizationId = () => {
+  let random;
+
+  do {
+    random = Math.floor(Math.random() * 1000) * Math.floor(Math.random() * 1000);
+  } while (random < 100000 && random < 999999);
+
+  return random;
+};
+
 routes.get('/', (request, response) =>
   response.json({ message: "Hello World!" }));
 
@@ -13,18 +23,45 @@ routes.get('/status', async (request, response) => {
   });
 });
 
-routes.post('/purchases', async (request, response) => {
-  console.log(request.body);
+routes.get('/purchases', async (request, response) => {
+  const { total_amount, } = request.body;
+  
+  const { data } = await api.post('/purchases');
+  console.log(data);
 
-  return response.json({ ok: "ok" });
+  return response.json({
+    message: "Operação realizada com sucesso.",
+    code: 0,
+    authorization_id: authorizationId(),
+    balance: {
+      amount: 123,
+      currency_code: 986
+    }
+  });
 });
 
 routes.post('/purchases/cancel', async (request, response) => {
-  return response.json({ ok: "ok" });
+  return response.json({
+    message: "Operação realizada com sucesso.",
+    code: 0,
+    authorization_id: authorizationId(),
+    balance: {
+      amount: 123,
+      currency_code: 986
+    }
+  });
 });
 
-routes.post('//chargebacks', async (request, response) => {
-  return response.json({ ok: "ok" });
+routes.post('/chargebacks', async (request, response) => {
+  return response.json({
+    message: "Operação realizada com sucesso.",
+    code: 0,
+    authorization_id: authorizationId(),
+    balance: {
+      amount: 123,
+      currency_code: 986
+    }
+  });
 });
 
 module.exports = routes;
